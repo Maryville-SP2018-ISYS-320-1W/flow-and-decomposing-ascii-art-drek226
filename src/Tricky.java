020e7d0da2371384693aba7d64209d10cac9447d
Mike Magic <mikemagic@Thes-MacBook-Pro.local>
2018-03-16 10:47:09 -0500

    Tricky

--- src/Tricky.java
+++ src/Tricky.java
@@ -4,14 +4,28 @@
   	Date: 
 */
 
-// 1. Your predicted output 
+// 1. Your predicted output - done with main, message1, message2, done with message 2
+
+
+public class Tricky {public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
+
+ }
+
+// 3. Were you correct? Explain any differences - NO, I thought the "done with main" would be first and there is an extra "this is message1
  
 
-public class Tricky {
-
-}
-
-// 3. Were you correct? Explain any differences
- 
- 
 
