1472d435339732778d8f2de822eaf4e888fe26f4
Mike Magic <mikemagic@Thes-MacBook-Pro.local>
2018-03-18 13:32:57 -0500

    starfigure
    stars

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,55 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Derek Rieger
+  	Date: March 18th 2018
 */
 
-// 4. Your pseudocode algorithm for how to break down the figure
+// 4. Your pseudocode algorithm for how to break down the figure: 
+// main (draw star figure)
+// drawfigure1 -  
+// line break
+// drawfigure2 - 
+// line break
+// drawfigure3 - 
 
 
 public class StarFigures {
 
 	public static void main(String[] args) {
-
+		figure1();
+		System.out.println();
+		figure2();
+		System.out.println();
+		figure3();
 	}
+	
+	public static void figure1() {
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+	    System.out.println(" * * ");
+   }
 
+	public static void figure2() {
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+	    System.out.println(" * * ");
+	    System.out.println("*****");
+		System.out.println("*****");
+	}
+	
+	public static void figure3() {
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+	    System.out.println(" * * ");
+	}
+	
 }
